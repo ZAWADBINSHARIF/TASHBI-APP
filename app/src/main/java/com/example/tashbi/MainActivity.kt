c9dea956eb7dc1d4119b64afaed9d12fdea86bdb
ZAWAD SHARIF <ZAWADSHARIF@github.com>
2020-06-13 13:08:35 +0600

    Constraint Layout

---
+++ app/src/main/java/com/example/tashbi/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.tashbi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        counter_btn.setOnClickListener {
+            val op = counterNumber.text.toString().toInt() + 1
+            counterNumber.text = "$op"
+        }
+        restoreBtn.setOnClickListener {
+            val op = "00"
+            counterNumber.text = op
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
